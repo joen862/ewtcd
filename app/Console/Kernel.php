0e2cddbeac55c8c77a538dd0ff914d22a33cfcef
Jeroen <jsentel@gmail.com>
2022-09-29 01:23:43 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,10 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Wallet;
+use App\Models\Supply;
+use App\Models\Market;
+use App\Models\Bridged;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,6 +20,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function() {
+            $market = new Market;
+            $market->updateMarketData();
+        })->everyMinute();
+
     }
 
     /**
