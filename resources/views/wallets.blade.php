21d627e32c099164d52326bcf89cc1f877eaf579
Jeroen <jsentel@gmail.com>
2022-12-18 02:24:17 +0100

    Update wallets.blade.php

--- resources/views/wallets.blade.php
+++ resources/views/wallets.blade.php
@@ -8,6 +8,7 @@
                     <thead>
                         <tr>
                             <!--<th style="width:10rem;">Category</th>-->
+                            <th style="width:20rem;">Category</th>
                             <th style="width:20rem;">Label</th>
                             <th style="width:10rem;">Address</th>
                             <th style="width:10rem;">Start Balance <i class="fa fa-normal fa-circle-info" title="This is the amount that the wallet initially received from the holding contract (token allocation) or from such wallet (in case of Founder2)"></th>
@@ -19,6 +20,7 @@
                         @foreach($wallets as $wallet)
                         <tr>
                             <!--<td>{{$wallet['category']}}</td>-->
+                            <td>{{$wallet['category']}}</td>
                             <td>{{$wallet['label']}}</td>
                             <td class="text-left "><a href="https://explorer.energyweb.org/address/{{$wallet['address']}}/coin-balances#address-tabs" target="_blank">{{substr($wallet['address'],0,8)}}-{{substr($wallet['address'],36,6)}}</a><span style="display:none;">{{$wallet['address']}}</span></td>
                             <td class="monospace text-right">{{number_format($wallet['token_allocation'])}}</td>
