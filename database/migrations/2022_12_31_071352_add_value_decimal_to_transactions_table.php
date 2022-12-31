647f2e368f35143dfaa6dd2bcdef354c6091973a
Jeroen <jsentel@gmail.com>
2022-12-31 08:46:57 +0100

    db decimal

---
+++ database/migrations/2022_12_31_071352_add_value_decimal_to_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->decimal('value_decimal', 30, 18)->nullable()->after('value');
+            $table->index('hash');
+            $table->index('from');
+            $table->index('to');
+            $table->index('value_decimal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('value_decimal');
+            $table->dropIndex('hash');
+            $table->dropIndex('from');
+            $table->dropIndex('to');
+            $table->dropIndex('value_decimal');
+        });
+    }
+};
