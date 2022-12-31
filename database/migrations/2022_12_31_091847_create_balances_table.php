bbb1920af282b0d6de38853c6f907adc8723de20
Jeroen <jsentel@gmail.com>
2022-12-31 10:39:42 +0100

    Create 2022_12_31_091847_create_balances_table.php

---
+++ database/migrations/2022_12_31_091847_create_balances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('balances', function (Blueprint $table) {
+            $table->bigIncrements('id'); // adds the primary key column
+            $table->timestamp('timestamp')->nullable();
+            $table->string('address');
+            $table->decimal('balance', 30, 18);
+            $table->index('timestamp');
+            $table->index('address');
+            $table->index('balance');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('balances');
+    }
+};
